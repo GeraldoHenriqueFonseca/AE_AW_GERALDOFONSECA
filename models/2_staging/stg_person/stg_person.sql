c73200d93235e1cf9cf7dd929ede26f28a2e7d07
Geraldo Fonseca <ghenfon@gmail.com>
2025-03-25 03:04:28 +0000

    hey

--- models/2_staging/stg_person/stg_person.sql
+++ models/2_staging/stg_person/stg_person.sql
@@ -3,9 +3,9 @@ with
         select 
             businessentityid
             , persontype
-            , firstname
-            , middlename
-            , lastname
+            , cast(firstname as string) as firstname
+            , cast(middlename as string) as middlename
+            , cast(lastname as string) as lastname
             , CONCAT(firstname, ' ', lastname) AS fullname
             , modifieddate
     from {{ source('stg_person', 'person') }}
